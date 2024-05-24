ac2e9572311298709b3ee850560f411058b8cec5
depicted-gaia <ncordoba@gaiaamazonas.org>
2024-05-23 19:59:12 -0500

    Scrolls y espacios vacíos arreglados

--- react/src/pages/estilos/global.ts
+++ react/src/pages/estilos/global.ts
@@ -2,10 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const EstiloGlobal = createGlobalStyle`
   body, html, #root {
-    margin: 0;
-    padding: 0;
     width: 100vw;
-    overflow: hidden;
+    margin: 0;
   }
 `;
 
