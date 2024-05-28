9adb590457dd4cd8f950550a1edd43303136e0dd
depicted-gaia <ncordoba@gaiaamazonas.org>
2024-05-28 18:16:59 -0500

    Creación de acceso único a BigQuery para la aplicación

--- react/fetch_secret.py
+++ react/fetch_secret.py
@@ -1,6 +1,6 @@
 from google.cloud import secretmanager
 from google.oauth2 import service_account
-import os
+import os, sys
 import logging
 
 
@@ -8,6 +8,7 @@ def fetch_secret(
     project_number, secret_id, version_id, destination_file, credentials_file
 ):
     logging.info("Initializing Secret Manager client with provided credentials")
+    logging.info(f"Credentials file: {credentials_file}")
     credentials = service_account.Credentials.from_service_account_file(
         credentials_file
     )
@@ -16,6 +17,7 @@ def fetch_secret(
     logging.info(f"Fetching secret from {name}")
     try:
         response = client.access_secret_version(request={"name": name}, timeout=30)
+        logging.info(f"Writing secret to {destination_file}")
         with open(destination_file, "wb") as f:
             f.write(response.payload.data)
         logging.info(f"Secret written to {destination_file}")
@@ -29,7 +31,9 @@ if __name__ == "__main__":
     project_number = os.environ.get("PROJECT_NUMBER")
     secret_id = os.environ.get("SECRET_ID")
     version_id = os.environ.get("VERSION_ID", "latest")
-    destination_file = os.environ.get("DESTINATION_FILE", "/app/service-account.json")
+    destination_file = os.environ.get(
+        "DESTINATION_FILE", "/app/sigeti-dee63dd3ec66.json"
+    )
     credentials_file = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
     if not credentials_file:
