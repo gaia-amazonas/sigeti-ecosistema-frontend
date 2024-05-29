770ef1ad153e24c9da8f3eb76f15fb27867b07ae
depicted-gaia <ncordoba@gaiaamazonas.org>
2024-05-28 19:32:50 -0500

    Creación de acceso único a BigQuery para la aplicación

--- react/fetch_secret.sh
+++ react/fetch_secret.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Setting Google Cloud project to $PROJECT_NUMBER..."
+gcloud config set project $PROJECT_NUMBER
+
 echo "Fetching secret from Secret Manager..."
 secret=$(gcloud secrets versions access latest --secret=$SECRET_ID)
 
