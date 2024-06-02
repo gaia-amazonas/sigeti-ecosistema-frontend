77781c995433e9054e0166d488141f8d6f7269c8
depicted-candela <nicalcoca@gmail.com>
2024-06-01 22:26:32 -0500

    simplicidad mejorada para cuando no hay internet

--- react/src/pages/index.tsx
+++ react/src/pages/index.tsx
@@ -1,24 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import EstiloGlobal from './estilos/global';
 import Boton, { BotonesContenedor, Titulo } from './estilos/boton';
 
 const Home: React.FC = () => {
   const [modo, establecerModo] = useState<'online' | 'offline'>('online');
+  const [isOnline, setIsOnline] = useState<boolean>(true);
 
-  const cambiarModo = () => {
-    establecerModo(modoPrevio => (modoPrevio === 'online' ? 'offline' : 'online'));
-    process.env.AMBIENTE = modo === 'online' ? 'offline' : 'online';
-  };
+  useEffect(() => {
+    const updateOnlineStatus = () => {
+      const onlineStatus = navigator.onLine;
+      setIsOnline(onlineStatus);
+      establecerModo(onlineStatus ? 'online' : 'offline');
+    };
+
+    if (typeof window !== 'undefined') {
+      updateOnlineStatus();
+      window.addEventListener('online', updateOnlineStatus);
+      window.addEventListener('offline', updateOnlineStatus);
+
+      return () => {
+        window.removeEventListener('online', updateOnlineStatus);
+        window.removeEventListener('offline', updateOnlineStatus);
+      };
+    }
+  }, []);
 
   return (
     <>
       <EstiloGlobal />
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
         <Titulo>Bienvenido a SIGETI</Titulo>
-        <button onClick={cambiarModo}>
-          {modo === 'online' ? 'Cambiar a modo Offline' : 'Cambiar a modo Online'}
-        </button>
+        {!isOnline ? (
+          <p>Offline</p>
+        ) : (
+          <p></p>
+        )}
         <BotonesContenedor>
           <Link href={{ pathname: '/consulta/alfanumerica/inicio', query: { modo } }} passHref>
             <Boton as="span">Seleccionar por Territorio y Comunidad</Boton>
