29b3a9932c655d283f0fc8bc7cf2b36d62917707
depicted-candela <nicalcoca@gmail.com>
2024-07-12 16:23:19 -0500

    Queries puestas como icono que se mueve

--- react/src/pages/index.tsx
+++ react/src/pages/index.tsx
@@ -1,13 +1,16 @@
 // src/pages/index.tsx
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-
+import { FaSearch } from 'react-icons/fa';
+import styled, { keyframes } from 'styled-components';
 import EstiloGlobal from './estilos/global';
-import Boton, { BotonesContenedor, Titulo } from './estilos/boton';
+import Header from './estilos/header';
+import Boton, { BotonesContenedor } from './estilos/boton';
 
 const Home: React.FC = () => {
   const [modo, establecerModo] = useState<'online' | 'offline'>('online');
   const [isOnline, setIsOnline] = useState<boolean>(true);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
 
   useEffect(() => {
     const updateOnlineStatus = () => {
@@ -28,31 +31,100 @@ const Home: React.FC = () => {
     }
   }, []);
 
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <>
       <EstiloGlobal />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          height: '100vh',
-          justifyContent: 'center'
-        }}
-      >
-        <Titulo>Bienvenido a SIGETI</Titulo>
+      <Container>
+        <Header>SIGETI</Header>
         {!isOnline ? <p>Offline</p> : <p></p>}
-        <BotonesContenedor>
-          <Link href={{ pathname: '/consulta/alfanumerica/inicio', query: { modo } }} passHref>
-            <Boton as="span">Seleccionar por Territorio y Comunidad</Boton>
-          </Link>
-          <Link href={{ pathname: '/consulta/espacial/inicio', query: { modo } }} passHref>
-            <Boton as="span">Consultar con Mapa</Boton>
-          </Link>
-        </BotonesContenedor>
-      </div>
+        <IconoHerramienta onClick={togglePopup}>
+          <FaSearch size={50} />
+        </IconoHerramienta>
+        {showPopup && (
+          <BotonesContenedor>
+            <AnimatedLink href={{ pathname: '/consulta/alfanumerica/inicio', query: { modo } }} passHref>
+              <Boton as="span">Temáticas</Boton>
+            </AnimatedLink>
+            <AnimatedLink href={{ pathname: '/consulta/espacial/inicio', query: { modo } }} passHref>
+              <Boton as="span">Gestión Documental Territorial</Boton>
+            </AnimatedLink>
+          </BotonesContenedor>
+        )}
+      </Container>
     </>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  justify-content: flex-start;
+  padding-top: 10px;
+`;
+
+const pulsate = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const IconoHerramienta = styled.div`
+  position: fixed;
+  bottom: 10rem;
+  right: 1rem;
+  cursor: pointer;
+  animation: ${pulsate} 1.5s infinite;
+
+  svg {
+    color: #4682b4;
+    transition: color 0.3s;
+  }
+
+  &:hover svg {
+    color: #0056b3;
+  }
+`;
+
+const radialAppear = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.5) translate(0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1) translate(var(--translate-x), var(--translate-y));
+  }
+`;
+
+const AnimatedLink = styled(Link)`
+  position: fixed;
+  --translate-x: 0;
+  --translate-y: 0;
+  animation: ${radialAppear} 0.5s ease-out forwards;
+
+  &:nth-child(1) {
+    --translate-x: 7rem;
+    --translate-y: -4rem;
+    animation-delay: 0s;
+  }
+
+  &:nth-child(2) {
+    --translate-x: 7rem;
+    --translate-y: -10rem;
+    animation-delay: 0.1s;
+  }
+`;
 
 export default Home;
